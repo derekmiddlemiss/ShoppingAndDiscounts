63005baf04829e74faf9fb54920fc9a5f8bfebbd
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-08-27 19:15:45 +0100

    No loyalty card on create, but can add a loyalty card afterwards

--- specs/TestShoppingBasket.java
+++ specs/TestShoppingBasket.java
@@ -3,6 +3,8 @@ import shopping.*;
 import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestShoppingBasket {
 
@@ -22,6 +24,17 @@ public class TestShoppingBasket {
     @Test
     public void testEmptyOnCreate(){
         assertEquals( 0, fredsBasket.getNumberItemsInBasket() );
+    }
+
+    @Test
+    public void testNoLoyaltyCardOnCreate(){
+        assertFalse( fredsBasket.getLoyaltyCard() );
+    }
+
+    @Test
+    public void testAddLoyaltyCard(){
+        fredsBasket.customerHasLoyaltyCard();
+        assertTrue( fredsBasket.getLoyaltyCard() );
     }
 
 }
